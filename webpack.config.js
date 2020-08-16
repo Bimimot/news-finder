3321cb2c863079d58677c1f28d8dc922da12eae1
Stepan Popov <bimimot@yandex.ru>
2020-08-16 16:10:43 +0300

    Added rule for fonts

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
             },
         ]
     },
+    {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]'
+    }
     ]
   },
   plugins: [
