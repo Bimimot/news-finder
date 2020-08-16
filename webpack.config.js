8e1754e19346cf895a85b48de526a2152dd70a0a
Stepan Popov <bimimot@yandex.ru>
2020-08-16 14:49:41 +0300

    Added html-webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,35 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключили плагин
 
 module.exports = {
-    entry: {
-        main: './src/index.js'
-    },
+    entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
     },
     module: {
-        rules: [{
+        rules: [
+            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
                 }
             },
-            {
+    {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            }
+                use:  [MiniCssExtractPlugin.loader, 'css-loader']
+             }
         ]
     },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'style.css'
+        new MiniCssExtractPlugin({filename: 'style.css'}),
+        new HtmlWebpackPlugin({ // настроили плагин
+            inject: false,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html'
         })
     ]
 };
