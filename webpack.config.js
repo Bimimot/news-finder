307888675ce6cba052c196d73b047d0b4cc52ab2
Stepan Popov <bimimot@yandex.ru>
2020-08-16 14:58:00 +0300

    Installed plugin for CSS-minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,37 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключили плагин
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // добавили плагин
 module.exports = {
-    entry: { main: './src/index.js' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-    {
-                test: /\.css$/,
-                use:  [MiniCssExtractPlugin.loader, 'css-loader']
-             }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({filename: 'style.css'}),
-        new HtmlWebpackPlugin({ // настроили плагин
-            inject: false,
-            hash: true,
-            template: './src/index.html',
-            filename: 'index.html'
-        })
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] // добавили минификацию CSS
+      }
     ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ //
+      filename: 'style.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
 };
