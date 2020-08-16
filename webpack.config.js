4e74fb86c02dd5a5d48f3c1211df403b37d3895c
Stepan Popov <bimimot@yandex.ru>
2020-08-16 14:48:04 +0300

    Added minification's plugins for CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,31 @@
-
-const path = require('path'); // подключаем path к конфигу вебпак для сборки абсолютного пути
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: { main: './src/index.js' },
+    entry: {
+        main: './src/index.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
-    }
-}
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ]
+};
