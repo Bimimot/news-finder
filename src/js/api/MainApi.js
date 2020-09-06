41a58c90da64b73b28c3370b39d1a380091bb752
Stepan Popov <bimimot@yandex.ru>
2020-09-06 17:24:22 +0300

    Change url for API

--- src/js/api/MainApi.js
+++ src/js/api/MainApi.js
@@ -1,7 +1,7 @@
 export default class MainApi {
   constructor() {
-    // this.url = 'http://newsfinder.tk/api';
-    this.url = 'http://localhost:3000/api';
+    this.url = 'http://newsfinder.tk/api';
+    // this.url = 'http://localhost:3000/api';
 
   }
 
